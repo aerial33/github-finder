8ebcc5daa733a34d4e0436bcb4c89454904d8ca0
Fouad <perceval@protonmail.com>
2021-11-25 15:34:05 +0100

    Add alert message for search user

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navabar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
 
 function App() {
   const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [search, setSearch] = useState("");
+  const [alert, setAlert] = useState();
 
   const searchUsers = async () => {
     try {
@@ -16,7 +18,7 @@ function App() {
         `https://api.github.com/search/users?q=${search}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
       );
       const data = await response.json();
-      console.log(data.items);
+      // console.log(data.items);
       if (data && !data.error) {
         setUsers(data.items);
       }
@@ -30,6 +32,9 @@ function App() {
   const handleSearchChange = (e) => setSearch(e.target.value);
   const handleSearchSubmit = (e) => {
     e.preventDefault();
+    if (search === "") {
+      setAlert({ msg: "Please enter something", type: "light" });
+    }
     setSearch(search);
     searchUsers();
     setSearch("");
@@ -38,16 +43,25 @@ function App() {
     setUsers([]);
     setIsLoading(false);
   };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAlert("");
+    }, 5000);
+  }, [alert]);
+
   return (
     <div className="App">
       <Navabar />
       <div className="container">
+        {alert && <Alert alert={alert} />}
         <Search
           onSearchChange={handleSearchChange}
           search={search}
           onSearchSubmit={handleSearchSubmit}
           onClearClick={handleClearClick}
-          showClear={users.length > 0 ? true : false}
+          showClear={undefined !== users && users.length > 0 ? true : false}
+          setAlert={alert}
         />
         <Users users={users} loading={isLoading} />
       </div>
