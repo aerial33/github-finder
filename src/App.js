853b13e8ff219ce75d425a68105e6bdbed545715
Fouad <perceval@protonmail.com>
2021-11-26 23:22:13 +0100

    Set up Route with react-router-dom and add page user

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import "./App.css";
 import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navabar from "./components/layout/Navbar";
+import User from "./components/users/User";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [search, setSearch] = useState("");
   const [alert, setAlert] = useState();
 
+  // * Search Github users
   const searchUsers = async () => {
     try {
       setIsLoading(true);
@@ -29,6 +34,27 @@ function App() {
     }
   };
 
+  // * Get single Github user
+  const getUser = async (username) => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(
+        `https://api.github.com/users/${username}?&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      );
+      const data = await response.json();
+      // console.log(data.items);
+      if (data && !data.error) {
+        console.log(data);
+        setUser(data);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  // * Form Submit for the search
   const handleSearchChange = (e) => setSearch(e.target.value);
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -39,11 +65,14 @@ function App() {
     searchUsers();
     setSearch("");
   };
+
+  // * Clear users from state
   const handleClearClick = () => {
     setUsers([]);
     setIsLoading(false);
   };
 
+  // * set Alert 5s if null
   useEffect(() => {
     setTimeout(() => {
       setAlert("");
@@ -51,21 +80,47 @@ function App() {
   }, [alert]);
 
   return (
-    <div className="App">
-      <Navabar />
-      <div className="container">
-        {alert && <Alert alert={alert} />}
-        <Search
-          onSearchChange={handleSearchChange}
-          search={search}
-          onSearchSubmit={handleSearchSubmit}
-          onClearClick={handleClearClick}
-          showClear={undefined !== users && users.length > 0 ? true : false}
-          setAlert={alert}
-        />
-        <Users users={users} loading={isLoading} />
+    <Router>
+      <div className="App">
+        <nav>
+          <Navabar />
+        </nav>
+        <main>
+          <div className="container">
+            {alert && <Alert alert={alert} />}
+            <Routes>
+              <Route
+                exact
+                path="/"
+                element={
+                  <>
+                    <Search
+                      onSearchChange={handleSearchChange}
+                      search={search}
+                      onSearchSubmit={handleSearchSubmit}
+                      onClearClick={handleClearClick}
+                      showClear={
+                        undefined !== users && users.length > 0 ? true : false
+                      }
+                      setAlert={alert}
+                    />
+                    <Users users={users} loading={isLoading} />
+                  </>
+                }
+              />
+              <Route exact path="/about" element={<About />} />
+              <Route
+                exact
+                path="/user/:gitlogin"
+                element={
+                  <User getUser={getUser} user={user} loading={isLoading} />
+                }
+              />
+            </Routes>
+          </div>
+        </main>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/components/users/Useritem.js
+++ src/components/users/Useritem.js
@@ -1,26 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-const UserItem = ({ user }) => {
+const UserItem = ({ user: { login, avatar_url } }) => {
   return (
     <div className="card text-center">
       <img
-        src={user.avatar_url}
+        src={avatar_url}
         alt=""
         className="round-img"
         style={{ width: "60px" }}
       />
-      <h3>{user.login}</h3>
+      <h3>{login}</h3>
 
       <div>
-        <a
-          href={user.html_url}
-          target="_blank"
+        <Link
+          to={`/user/${login}`}
           className="btn btn-dark btn-sm my-1"
           rel="noreferrer"
         >
           More
-        </a>
+        </Link>
       </div>
     </div>
   );
